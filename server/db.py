b8669ade0178c7fdd52201b8e7a4a61a2da016e1
Charlytoc <charlyjchaconc@gmail.com>
2025-08-05 23:07:20 -0500

    fix problem with db

--- server/db.py
+++ server/db.py
@@ -9,21 +9,10 @@ from contextlib import asynccontextmanager, contextmanager
 # Cargar variables de entorno
 load_dotenv()
 
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_DB = os.getenv("POSTGRES_DB")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "127.0.0.1")
-POSTGRES_PORT = os.getenv("POSTGRES_HOST_PORT", "5432")
 
 # URLs para ambos engines
-ASYNC_DATABASE_URL = (
-    f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
-    f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-)
-SYNC_DATABASE_URL = (
-    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
-    f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-)
+ASYNC_DATABASE_URL = os.getenv("ASYNC_DATABASE_URL")
+SYNC_DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Engine y sessionmaker asíncronos
 async_engine = create_async_engine(ASYNC_DATABASE_URL, echo=False, future=True)
@@ -43,15 +32,18 @@ SyncSessionLocal = sessionmaker(
 # Base para los modelos
 Base = declarative_base()
 
+
 # Context manager asíncrono
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
         yield session
 
+
 @asynccontextmanager
 async def session_context():
     async with AsyncSessionLocal() as session:
         yield session
+
 
 # Context manager síncrono
 @contextmanager
